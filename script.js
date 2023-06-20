f622852401c0dff27759d50f3cbd5b08d7fae1e8
Manoj Saini <manojsaini513@gmail.com>
2023-06-20 10:47:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-function threeSum(arr) {
+function threeSum(a) {
 // write your code here
- let res=[]; 
-	arr.sort((a,b)=>(a-b))
-    for(let i=0;i<arr.length-2;i++){
-        //to remove duplicates
-        if( i>0 && arr[i]===arr[i-1]) continue;
+let n = a.length;
 
-        let j=i+1; let k=arr.length-1;
-        while(j<k){
-            let sum = arr[i] + arr[j] + arr[k];
-            if(sum===0){
-                res.push([arr[i],arr[j],arr[k]]);
-                //stop duplicates
-                while(arr[j]===arr[j+1]) j++;//or else we end up getting a duplicate triplet
-                while(arr[k]===arr[k+1]) k--;//or else we end up getting a duplicate triplet
-                j++;//even if they aren't equal we still incremenet j
-                k--;//even if they aren't equal we still decrement k
-            }
-            else if(sum<0){ //we increase the value of j
-                j++;
-            }
-            else{//if the sum value is more than zero we decrease the value of k
-                k--;
+    a.sort((a, b) => a - b);
+
+    let aa = [];
+    let vis = new Set();
+    for (let i = 0; i < n; i++) {
+        let t = -a[i]; // target;
+        for (let L = i + 1, R = n - 1; L < R; ) {
+            let sum = a[L] + a[R];
+            if (sum === t) {
+                let key = `${a[i]} ${a[L]} ${a[R]}`;
+                if (!vis.has(key)) {
+                    vis.add(key);
+                    aa.push([a[i], a[L], a[R]]);
+                }
+                L++;
+            } else if (sum < t) {
+                L++;
+            } else {
+                R--;
             }
         }
     }
-    return res;
+    return aa;
 };
   
 
