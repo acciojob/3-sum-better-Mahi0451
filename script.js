131d54ad625fa25547edb3bf4e1f2b78349e438a
Manoj Saini <manojsaini513@gmail.com>
2023-06-20 10:38:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function threeSum(arr, target) {
 // write your code here
-  
+ let start = i+1,end = arr.length -1;
+ while(start < end){
+       const sum = arr[i]+arr[start]+arr[end];
+     if(sum === 0){
+         ans.push([arr[i],arr[start],arr[end]]);
+         start++;
+         end--;
+         while(start<end && arr[start]===arr[start-1]) start++;
+         while(start<end && arr[end]=== arr[end+1])end-=1;
+     }
+     else if(sum < 0) start++;
+     else if(sum>0) end--;
+     }
 }
+return ans;
+};	
+  
+
 
 module.exports = threeSum;
