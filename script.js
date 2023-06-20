da1964a4229713ab60541d501c6db5ea0ef3fa77
Manoj Saini <manojsaini513@gmail.com>
2023-06-20 10:44:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 function threeSum(arr) {
 // write your code here
- let start = i+1,end = arr.length -1;
- while(start < end){
-       const sum = arr[i]+arr[start]+arr[end];
-     if(sum === 0){
-         ans.push([arr[i],arr[start],arr[end]]);
-         start++;
-         end--;
-         while(start<end && arr[start]===arr[start-1]) start++;
-         while(start<end && arr[end]=== arr[end+1])end-=1;
-     }
-     else if(sum < 0) start++;
-     else if(sum>0) end--;
-     }
-}
-return ans;
-};	
+ let res=[]; 
+	nums.sort((a,b)=>(a-b))
+    for(let i=0;i<nums.length-2;i++){
+        //to remove duplicates
+        if( i>0 && nums[i]===nums[i-1]) continue;
+
+        let j=i+1; let k=nums.length-1;
+        while(j<k){
+            let sum = nums[i] + nums[j] + nums[k];
+            if(sum===0){
+                res.push([nums[i],nums[j],nums[k]]);
+                //stop duplicates
+                while(nums[j]===nums[j+1]) j++;//or else we end up getting a duplicate triplet
+                while(nums[k]===nums[k+1]) k--;//or else we end up getting a duplicate triplet
+                j++;//even if they aren't equal we still incremenet j
+                k--;//even if they aren't equal we still decrement k
+            }
+            else if(sum<0){ //we increase the value of j
+                j++;
+            }
+            else{//if the sum value is more than zero we decrease the value of k
+                k--;
+            }
+        }
+    }
+    return res;
+};
   
 
 
